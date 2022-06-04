322585bd811241045a417bf0b8d9f2ca3e46474b
Ambili das v s <ambili.kan@gmail.com>
2022-06-04 17:06:34 +0530

    Mood Analyzer Program is created

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoodAnalyzerProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWelcome to Mood Analyzer Problem");
+            Console.ReadKey();
+        }
+    }
+}
