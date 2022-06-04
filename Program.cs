c0c333b893d1da986d6f8ca7d204803e502d3330
Ambili das v s <ambili.kan@gmail.com>
2022-06-04 17:48:29 +0530

    Program to return mood is created and satisfied the 2 test cases

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace MoodAnalyzerProblem
         public static void Main(string[] args)
         {
             Console.WriteLine("\t\tWelcome to Mood Analyzer Problem");
+            MoodAnalyser moodAnalyserobj=new MoodAnalyser();
+            Console.WriteLine(moodAnalyserobj.AnalyseMood("I am in Sad mood"));
+            Console.WriteLine(moodAnalyserobj.AnalyseMood("I am in any mood"));
             Console.ReadKey();
         }
     }
